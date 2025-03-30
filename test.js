abcc8b0360fa00f891c56619c87fd4b2dbab9211
David Ashe <davidmashe@gmail.com>
2025-03-30 15:36:47 -0400

    formatted js test file

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
 const {
-	env: {
-		REPETITIONS = '25'
-	}
+  env: {
+    REPETITIONS = '25'
+  }
 } = process;
 
 let repetitions;
 try {
-	repetitions = parseInt(REPETITIONS)
+  repetitions = parseInt(REPETITIONS)
 } catch (err) {
-	console.error(err)
-	process.exit(1);
+  console.error(err)
+  process.exit(1);
 }
 
 const headers = {
-	'Content-Type': 'application/json',
+  'Content-Type': 'application/json',
 };
 
 const request = url => fetch(url, { headers }).then(r => r.json());
@@ -23,85 +23,85 @@ const localhostRequest = () => request('http://localhost:8080/');
 const getSeconds = () => new Date().getTime();
 
 const results = {
-	'unix': {
-		entries: [],
-	},
-	'localhost': {
-		entries: [],
-	}
+  'unix': {
+    entries: [],
+  },
+  'localhost': {
+    entries: [],
+  }
 };
 
 const averagesSummer = (accumulator = 0, resultEntry) => {
-	const { duration = 0 } = resultEntry;
+  const { duration = 0 } = resultEntry;
 
-	return accumulator + duration;
+  return accumulator + duration;
 };
 
 const report = (resultsObject) => {
 
-	const {
-		unix: {
-			entries: unixEntries,
-		},
-		localhost: {
-			entries: localhostEntries,
-		},
-		globalStart,
-		globalEnd,
-	} = resultsObject;
+  const {
+    unix: {
+      entries: unixEntries,
+    },
+    localhost: {
+      entries: localhostEntries,
+    },
+    globalStart,
+    globalEnd,
+  } = resultsObject;
 
-	const averageUnix = unixEntries.reduce(averagesSummer, 0) / unixEntries.length;
-	const averageLocalhost = localhostEntries.reduce(averagesSummer, 0) / localhostEntries.length;
+  const averageUnix = unixEntries.reduce(averagesSummer, 0) / unixEntries.length;
+  const averageLocalhost = localhostEntries.reduce(averagesSummer, 0) / localhostEntries.length;
 
-	resultsObject = {
-		averageUnix,
-		averageLocalhost,
-		totalTestDuration: globalEnd - globalStart, 
-	}
+  resultsObject = {
+    averageUnix,
+    averageLocalhost,
+    totalTestDuration: globalEnd - globalStart, 
+  }
 
-	const formatted = JSON.stringify(resultsObject, null, 2)
-	console.log(formatted);
+  const formatted = JSON.stringify(resultsObject, null, 2)
+  console.log(formatted);
 }
 
 const testWithRepetition = (key, requestFunction, promisesList) => {
 
-	console.log(`starting ${key} repetitions...`);
+  console.log(`starting ${key} repetitions...`);
 
-	for (let i = 0; i < repetitions; i++) {
+  for (let i = 0; i < repetitions; i++) {
 
-		console.log(`${key} ${i + 1}`);
-		const start = getSeconds();
+    console.log(`${key} ${i + 1}`);
+    const start = getSeconds();
 
-		const promise = requestFunction().then((response) => {
-			console.log(`request ${key} ${i} succeeded`);
-			const end = getSeconds();
-			results[key].entries.push({ start, end, response, duration: end - start });
-		});
-		promisesList.push(promise);
-	}	
+    const promise = requestFunction().then((response) => {
+      console.log(`request ${key} ${i} succeeded`);
+      const end = getSeconds();
+      results[key].entries.push({ start, end, response, duration: end - start });
+    });
+    promisesList.push(promise);
+  } 
 }
 
 const main = async () => {
-	const globalStart = getSeconds();
+  const globalStart = getSeconds();
 
-	const promises = [];
+  const promises = [];
 
-	testWithRepetition('unix', unixRequest, promises);
+  testWithRepetition('unix', unixRequest, promises);
 
-	testWithRepetition('localhost', localhostRequest, promises);
+  testWithRepetition('localhost', localhostRequest, promises);
 
-	try {
-		await Promise.all(promises);
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    await Promise.all(promises);
+  } catch (error) {
+    console.log(error);
+  }
 
-	const globalEnd = getSeconds();
+  const globalEnd = getSeconds();
 
-	results.globalStart = globalStart;
-	results.globalEnd = globalEnd;
+  results.globalStart = globalStart;
+  results.globalEnd = globalEnd;
 
-	report(results);
+  report(results);
 };
 
 main();
